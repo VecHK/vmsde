78d181dcf61e83d35154bce7ed3bfec8b7e53faf
vec <vec@torzo.club>
2021-06-17 09:55:02 +0800

    [add] 胜利/失败 后的界面

--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -1,13 +1,13 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import './index.css'
 
-import CreateVMS, { Cell, countBomb, countMark } from 'src/vms-logic'
+import CreateVMS, { getVMSStatus } from 'src/vms-logic'
 
 import { Dashboard } from './components/Dashboard'
 import GameMap from './components/GameMap'
-import GameCell from './components/GameCell'
+import { GameStatus } from './components/GameStatus'
 
-const WIDTH = 16
+const WIDTH = 12
 const HEIGHT = 16
 const BOMB_NUM = Math.floor(WIDTH * HEIGHT * 0.15)
 
@@ -16,52 +16,20 @@ export default () => {
     CreateVMS({ width: WIDTH, height: HEIGHT, bombNumber: BOMB_NUM })
   )
 
-  const { width, height, map } = vms
-  const { matrix } = map
+  const status = useMemo(() => getVMSStatus(vms.map.matrix), [vms.map.matrix])
 
   return (
     <article>
       <Dashboard vms={vms} />
+
       <GameMap vms={vms} setVMS={setVMS} />
 
-      <button
-        onClick={() => {
-          setVMS(
-            CreateVMS({ width: WIDTH, height: HEIGHT, bombNumber: BOMB_NUM })
-          )
-        }}
-      >
-        Replay
-      </button>
-      <button
-        onClick={() => {
-          const { matrix } = map
-
-          const flagMatrix = matrix
-            .map((cell) => Number(cell.mark === 'FLAG'))
-            .join('')
-          console.log('flagMatrix', flagMatrix)
-        }}
-      >
-        exportflagMatrix
-      </button>
-
-      <div className="row">
-        {([0, 1, 2, 3, 4, 5, 6, 7, 8] as Cell['neighborNumber'][]).map((n) => {
-          return (
-            <GameCell
-              key={n}
-              cell={{
-                id: 9,
-                isBomb: false,
-                mark: 'NONE',
-                neighborNumber: n,
-                isOpen: true,
-              }}
-            />
-          )
-        })}
-      </div>
+      <GameStatus
+        status={status}
+        vms={vms}
+        setVMS={setVMS}
+        cfg={{ WIDTH, HEIGHT, BOMB_NUM }}
+      />
     </article>
   )
 }
