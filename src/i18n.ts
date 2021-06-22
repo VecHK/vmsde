51633bac45d14ebfcb5a1c422b3dcc08f3f5e904
vec <vec@torzo.club>
2021-06-22 19:27:40 +0800

    [add] auto detect language

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 const resources = {
   ja: {
@@ -40,14 +41,26 @@ const resources = {
   },
 }
 
-i18n.use(initReactI18next).init({
-  resources,
-
-  lng: 'ja',
-
-  interpolation: {
-    escapeValue: false,
-  },
-})
-
 export default i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources,
+
+    detection: {
+      order: [
+        // 'querystring',
+        // 'cookie',
+        'localStorage',
+        // 'sessionStorage',
+        'navigator',
+        // 'htmlTag',
+        // 'path',
+        // 'subdomain',
+      ],
+    },
+
+    interpolation: {
+      escapeValue: false,
+    },
+  })
